4b60d1b56b7b860aaeadc9de49911492ca095101
ad.0520 <adrianchanyikhei@gmail.com>
2026-01-28 10:53:20 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+print("hello world")
+def add(x,y,):
+    z=x+y
+
+    print("z="+str(z))
+
+x=8
+y=9
+
+if x>y:
+    print("true")
+else:
+    print("false")
+fruits=["durian","apple", "orange"]
+print(len(fruits))
+import random
+for i in range(10):
+ print(random.randint(0,100))
+
+
